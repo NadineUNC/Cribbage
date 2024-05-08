561742245a1ec4aae34b0a468d9544c0f2e73561
Nadine Hughes <nadie@unc.edu>
2024-05-08 12:21:34 -0400

    starting over

--- src/java/src/com.cribbagegame/model/Card.java
+++
@@ -1,8 +0,0 @@
-package com.cribbagegame.model;
-
-public interface Card {
-  int getValue();
-  int getRun_order();
-  String getSuit();
-  String getName();
-}
--- src/java/src/com.cribbagegame/model/Model.java
+++
@@ -1,5 +0,0 @@
-package com.cribbagegame.model;
-
-public interface Model {
-
-}
